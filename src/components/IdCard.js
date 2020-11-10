f7b3d895879e663d80e58f0ee8b686caa4cdc384
Andreu Martínez <martinez.andreu@gmail.com>
2020-11-10 21:19:50 +0100

    [WDFT-BCN-0920] Andreu Mtez iteration 1

---
+++ src/components/IdCard.js
@@ -0,0 +1,19 @@
+import React from 'react';
+// import './App.css';
+
+const IdCard = (props) => {
+    return (
+        <div className="item">
+            <img src={props.picture} alt="image" />
+            <div className="rest-item">
+                <p><b>First Name: </b><span>{props.firstName}</span></p>
+                <p><b>Last Name: </b><span>{props.lastName}</span></p>
+                <p><b>Gender: </b><span>{props.gender}</span></p>
+                <p><b>Height: </b><span>{props.height/100}m</span></p>
+                <p><b>Birth: </b><span>{props.birth}</span></p>
+            </div>
+        </div>
+    );
+};
+
+export default IdCard;
